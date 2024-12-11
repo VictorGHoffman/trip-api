ee4b46a0262e5d039626129e6637b664ff176fdc
VictorGHoffman <victor.ghoff@gmail.com>
2024-12-11 11:43:01 -0300

    refactor: controllers to new generic

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,63 +1,10 @@
-const express = require("express")
-const app = express.Router()
-const HttpStatus = require("../utils/HttpStatus")
-const getLimitAndPage = require("../utils/PageLimit")
+const BaseController = require("./BaseController")
 const RoomService = require("../services/RoomService")
 
-app.get("/rooms", async (req, res) => {
-    const { limit, page } = getLimitAndPage(req.query.limit, req.query.page)
-    try {
-        const rooms = await RoomService.findAll(limit, page, req.query.sort)
-        res.send(rooms)
-    } catch (error) {
-        res.status(error.status || HttpStatus.NOT_FOUND)
-        res.send(error.message)
+class RoomController extends BaseController {
+    constructor () {
+        super("rooms", RoomService)
     }
-})
+}
 
-app.get("/rooms/:id", async (req, res) => {
-    const id = req.params.id
-    try {
-        const room = await RoomService.findById(id)
-        res.send(room)
-    } catch (error) {
-        res.status(error.status || HttpStatus.NOT_FOUND)
-        res.send(error.message)
-    }
-})
-
-app.post("/rooms", async (req, res) => {
-    const room = req.body
-    try {
-        await RoomService.create(room)
-        res.sendStatus(HttpStatus.CREATED)
-    } catch (error) {
-        res.status(error.status || HttpStatus.BAD_REQUEST)
-        res.send(error.message)
-    }
-})
-
-app.put("/rooms/:id", async (req, res) => {
-    const id = req.params.id
-    const room = req.body
-    try {
-        await RoomService.update(id, room)
-        res.sendStatus(HttpStatus.OK)
-    } catch (error) {
-        res.status(error.status || HttpStatus.NOT_FOUND)
-        res.send(error.message)
-    }
-})
-
-app.delete("/rooms/:id", async (req, res) => {
-    const id = req.params.id
-    try {
-        await RoomService.delete(id)
-        res.sendStatus(HttpStatus.NO_CONTENT)
-    } catch (error) {
-        res.status(error.status || HttpStatus.NOT_FOUND)
-        res.send(error.message)
-    }
-})
-
-module.exports = app
+module.exports = new RoomController().app
--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,61 +1,10 @@
-const express = require("express")
-const app = express.Router()
-const HttpStatus = require("../utils/HttpStatus")
-const getLimitAndPage = require("../utils/PageLimit")
+const BaseController = require("./BaseController")
 const UserService = require("../services/UserService")
 
-app.get("/users", async (req, res) => {
-    let { limit, page } = getLimitAndPage(req.query.limit, req.query.page)
-    const users = await UserService.findAll(limit, page, req.query.sort)
-
-    res.send(users)
-})
-
-app.get("/users/:id", async (req, res) => {
-    const id = req.params.id
-
-    try {
-        const user = await UserService.findById(id)
-        res.send(user)
-    } catch (error) {
-        res.status(error.status || HttpStatus.NOT_FOUND)
-        res.send(error.message)
+class UserController extends BaseController {
+    constructor () {
+        super("users", UserService)
     }
-})
+}
 
-app.post("/users", async (req, res) => {
-    const user = req.body
-    try {
-        await UserService.create(user)
-        res.sendStatus(HttpStatus.CREATED)
-    }
-    catch (error) {
-        res.status(error.status || HttpStatus.BAD_REQUEST)
-        res.send(error.message)
-    }
-})
-
-app.put("/users/:id", async (req, res) => {
-    const id = req.params.id
-    const user = req.body
-    try {
-        await UserService.update(id, user)
-        res.sendStatus(HttpStatus.OK)
-    } catch (error) {
-        res.status(error.status || HttpStatus.NOT_FOUND)
-        res.send(error.message)
-    }
-})
-
-app.delete("/users/:id", async (req, res) => {
-    const id = req.params.id
-    try {
-        await UserService.delete(id)
-        res.sendStatus(HttpStatus.NO_CONTENT)
-    } catch (error) {
-        res.status(error.status || HttpStatus.NOT_FOUND)
-        res.send(error.message)
-    }
-})
-
-module.exports = app
+module.exports = new UserController().app
